e170e966f0d051ed1511b6c915dd708c3ada38d6
gabyzzzzz <gabrielzamifrache@gmail.com>
2025-07-03 15:57:06 +0300

    lib

---
+++ lib/pch.h
@@ -0,0 +1,25 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include <windows.h>
+#include <wincon.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <math.h>
+#include <algorithm>
+#include <functional>
+#include <thread>
+#include <atomic>
+#include <conio.h>
+#include <queue>
+#include <cmath>
+#include <mutex>
+#include <cstring>
+#include <unordered_set>
+#include <sstream>
+#include <io.h>
+#include <fcntl.h>
+#include "jaft.h"
+
+#endif 
